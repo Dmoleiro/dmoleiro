7a0501dd8bf887515c4c81127538192329b002bf
Daniel Ferreira <daniel.ferreira@ingg.com>
2019-10-14 06:22:53 +0100

    started implementing download button.

---
+++ src/components/downloadCV.js
@@ -0,0 +1,14 @@
+//V-DanielFerreira.pdf
+import React, { Component } from 'react';
+import '../styles/downloadCV.css';
+
+class DownloadCV extends Component {
+  render() {
+    return (
+      <div className="triangle-bottomright">
+        Download
+      </div>
+    );
+  }
+}
+export default DownloadCV;
--- src/containers/landing.js
+++ src/containers/landing.js
@@ -5,8 +5,35 @@ import '../styles/landing.css';
 import TitleBar from "../components/titleBar";
 import WorkHistory from "../components/workHistory";
 import WelcomeScreen from "../components/welcomeScreen";
+import DownloadCV from "../components/downloadCV";
 
 class Landing extends Component {
+  componentDidMount() {
+    this.scrollWait = 1000;
+    this.lastScrollFireTime = Date.now();
+    window.addEventListener("scroll", this._showDownload.bind(this));
+  }
+
+  componentWillUnmount() {
+     window.removeEventListener("scroll", this._showDownload.bind(this));
+  }
+
+  _showDownload() {
+    if ((this.lastScrollFireTime + this.scrollWait - Date.now()) < 0) {
+      let titleBar = document.getElementById('title-bar');
+      if (titleBar !== null && titleBar !== undefined) {
+        let bounding = titleBar.getBoundingClientRect();
+        if (bounding.top >= 0 && bounding.left >= 0 &&
+          bounding.right <= window.innerWidth && bounding.bottom <= window.innerHeight) {
+        	console.log('In the viewport!');
+        } else {
+        	console.log('Not in the viewport... whomp whomp');
+        }
+      }
+      this.lastScrollFireTime = Date.now();
+    }
+  }
+
   render(){
     return (
         <div className="mainContainer">
@@ -15,6 +42,7 @@ class Landing extends Component {
             <Introduction />
             <InfoSlider store={this.props.store}/>
             <WorkHistory />
+            <DownloadCV />
         </div>
     );
   }
