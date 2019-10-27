d94f821c45928b9a89e3a21883577e69ee635be3
Daniel Ferreira <daniel.ferreira@ingg.com>
2019-10-27 23:19:47 +0000

    finished styling projects, added new background.

--- src/components/projects.js
+++ src/components/projects.js
@@ -32,8 +32,8 @@ class Projects extends Component {
                                 <div className="text-shadow">PixiJS Game</div>
                                 <img className="tile" alt='dsantos' src={process.env.PUBLIC_URL + '/assets/projects/unnamed.png'}/>
                             </div>
-                            <div className="back" onClick={() => window.open('https://dmoleiro.github.io/UnNamed/')}>
-                                <div className="button">
+                            <div className="back">
+                                <div className="button" onClick={() => window.open('https://dmoleiro.github.io/UnNamed/')}>
                                     <span>Visit site</span>
                                     <svg>
                                         <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
