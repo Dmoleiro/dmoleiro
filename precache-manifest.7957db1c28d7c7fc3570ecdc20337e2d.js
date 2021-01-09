d622b34449c6611365aee1e558b02e3f131a4066
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-09 01:31:24 +0000

    Deploy Dmoleiro/dmoleiro to github.com/Dmoleiro/dmoleiro.git:gh-pages

---
+++ precache-manifest.7957db1c28d7c7fc3570ecdc20337e2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aab10fa360d7758b4057e1cb2cef3e",
+    "url": "/dmoleiro/index.html"
+  },
+  {
+    "revision": "a1c519b1bd498c1bccd1",
+    "url": "/dmoleiro/static/css/main.48c12497.chunk.css"
+  },
+  {
+    "revision": "b35f9e3c6146ba8096ef",
+    "url": "/dmoleiro/static/js/2.d5c0409e.chunk.js"
+  },
+  {
+    "revision": "a1c519b1bd498c1bccd1",
+    "url": "/dmoleiro/static/js/main.1ff40e8d.chunk.js"
+  },
+  {
+    "revision": "b4c30ad929433f17052f",
+    "url": "/dmoleiro/static/js/runtime-main.f3ed70a1.js"
+  }
+]);
